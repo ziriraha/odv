3c78d2a4ff55401515b22cdac137a661f1f5e1cb
Ziri <45337531+ziriraha@users.noreply.github.com>
2025-11-26 19:56:29 +0100

    feat(switch): better output
    ordered and with new style

--- cmd/switch.go
+++ cmd/switch.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/spf13/cobra"
 	"github.com/ziriraha/odv/internal"
@@ -15,16 +16,30 @@ var switchCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
 		version := internal.DetectVersion(args[0])
 		internal.Debug.Printf("switchCmd: version '%v' was detected", version)
+		var repoBranch sync.Map
 		internal.ForEachRepository(func (i int, repoName string, repository *internal.Repository) {
 			branchName := args[0]
 			if !repository.BranchExists(branchName) {
 				branchName = version
 				if !repository.BranchExists(branchName) { branchName = repository.DefaultBranch }
 			}
-			fmt.Printf("Switching '%v' to branch '%v'\n", repository.Color(repoName), branchName)
-			err := repository.SwitchBranch(branchName)
-			if err != nil { 
-				internal.Error.Printf("in repository %v: switching to branch '%v': %v", repoName, branchName, err) 
+			repoBranch.Store(repoName, branchName)
+		}, true)
+
+		var wg sync.WaitGroup
+		wg.Go(func() {
+			internal.ForEachRepository(func (i int, repoName string, repository *internal.Repository) {
+				branchName, _ := repoBranch.Load(repoName)
+				fmt.Printf("[%s] Switching to '%s'\n", repository.Color(repoName), branchName)
+			}, false)
+		})
+
+		internal.ForEachRepository(func (i int, repoName string, repository *internal.Repository) {
+			branchName, _ := repoBranch.Load(repoName)
+			err := repository.SwitchBranch(branchName.(string))
+			wg.Wait()
+			if err != nil {
+				internal.Error.Printf("in repository %v: %v", repoName, err) 
 			}
 		}, true)
 	},
